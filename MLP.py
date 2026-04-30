30d030d095d825d84bfaf62fddd879332b3bacb8
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2026-04-29 23:35:25 -0600

    Creada estructura del modelo

--- MLP.py
+++ MLP.py
@@ -1,9 +1,27 @@
 import numpy as np
 
-#Función de activación no lineal: sigmoide y su derivada
+#Función de activación sigmoide y su derivada
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
 
 def sigmoid_prime(z):
     s = sigmoid(z)
     return s * (1.0 - s)
+
+
+#Red neuronal multicapa (MLP)
+class MLP:
+    def __init__(self, X, n_hidden, n_output):
+        a=a
+    
+    #Paso hacia delante
+    def forward(self, X):
+        a=a
+    
+    #Paso hacia atrás
+    def backward(self, X, y):
+        a=a
+
+    #Entrenamiento
+    def train(self, X, y, epochs=200):
+        a=a
